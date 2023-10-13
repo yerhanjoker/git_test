9befca1469ecd8cf2c41b4340f75bc1f25d0f097
yerhanjoker <147694785+yerhanjoker@users.noreply.github.com>
2023-10-13 15:53:30 +0800

    Update sharetogithub.java

--- src/sharetogithub.java
+++ src/sharetogithub.java
@@ -1,6 +1,7 @@
 public class sharetogithub {
     public static void main(String[] args) {
         System.out.println("this is my first share project to github");
-        System.out.println("this is my first share project to github");
+        System.out.println("this is my first share push to github");
+        System.out.println("this is my first share push to github");
     }
 }
