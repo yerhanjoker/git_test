1ebe409045fa5e310930cfb6660d0e1b30f0daf8
yerhanjoker <1575058976@qq.com>
2023-10-13 15:49:45 +0800

    pushV1

--- src/sharetogithub.java
+++ src/sharetogithub.java
@@ -1,5 +1,5 @@
 public class sharetogithub {
     public static void main(String[] args) {
-        System.out.println("this is my first share project to github");
+        System.out.println("this is my first push project to github");
     }
 }
